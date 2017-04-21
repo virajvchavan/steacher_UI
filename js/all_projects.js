aa3dd2342695adebf85728ae190afefd41bdf39f
Viraj <virajc@live.com>
2017-04-21 17:30:43 +0530

    show skills associated with all projects

--- js/all_projects.js
+++ js/all_projects.js
@@ -3,15 +3,23 @@ var projectsRef = firebase.database().ref().child("projects");
 //show the projects
 projectsRef.once("value", snap =>{
     snap.forEach(function(child_snapshot)
-{
-					var id = child_snapshot.key;
-					var name = child_snapshot.child("name").val();
-                    var description = child_snapshot.child("description").val();
-        
-                    console.log(name + id + description);
-            
-                    $("#all_projects").append('<div class="card card-inverse" style="background-color: #333; border-color: #333;"><div class="card-block"><a href="project.html?id='+ id +'"><h3 class="card-title">'+ name +'</h3></a><p class="card-text">'+ description +'</p></div></div></div>');
-        
-                    //<a href="skill.html"><li class="list-group-item">HTML</li></a>
-				});
+	{
+		var id = child_snapshot.key;
+		var name = child_snapshot.child("name").val();
+	    var description = child_snapshot.child("description").val();
+
+	    $("#all_projects").append('<div class="card card-inverse" style="background-color: #333; border-color: #333;"><div class="card-block"><a href="project.html?id='+ id +'"><h3 class="card-title">'+ name +'</h3></a><p id="'+  id +'" class="card-text">'+ description + '<br>');
+
+	    //show the skills
+		child_snapshot.child("skills").forEach(function(skills_snapshot)
+		{
+			var skill_id = skills_snapshot.val();
+			var skill_name = skills_snapshot.key;
+						                
+			$("#" + id).append(" <div class='badge badge-primary'> "+ skill_name +" </div> ");
+		});
+
+		$("#all_projects").append("</p></div></div></div>");
+                    
+	});
 });
